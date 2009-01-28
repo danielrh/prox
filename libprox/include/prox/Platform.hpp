bea5f10dc1297eca32dd8eda73a006fe49adb28c
Ewen Cheslack-Postava <ewencp@cs.stanford.edu>
2009-01-28 13:25:30 -0800

    Customize platform header for this project.
    * libprox/include/prox/Platform.hpp: Change naming to be specific to
      libprox, clean out a few unnecessary items, add some useful default
      includes.

--- libprox/include/prox/Platform.hpp
+++ libprox/include/prox/Platform.hpp
@@ -1,53 +1,56 @@
 #ifndef _PLATFORM_HPP_
 #define _PLATFORM_HPP_
 
-#define MERU_PLATFORM_WINDOWS 0
-#define MERU_PLATFORM_LINUX   1
-#define MERU_PLATFORM_MAC     2
-#define MERU_PLATFORM_BSD     3
+#define LIBPROX_PLATFORM_WINDOWS 0
+#define LIBPROX_PLATFORM_LINUX   1
+#define LIBPROX_PLATFORM_MAC     2
+#define LIBPROX_PLATFORM_BSD     3
 
 
 #if defined(__WIN32__) || defined(_WIN32)
-#define MERU_PLATFORM MERU_PLATFORM_WINDOWS
+#define LIBPROX_PLATFORM PLATFORM_WINDOWS
 #elif defined(__APPLE_CC__) || defined(__APPLE__)
-#define MERU_PLATFORM MERU_PLATFORM_MAC
-// some other packages (ogg) need this defined, and we're only supporting OS X
-#define __MACOSX__
+#define LIBPROX_PLATFORM PLATFORM_MAC
 #else
-#define MERU_PLATFORM MERU_PLATFORM_LINUX
+#define LIBPROX_PLATFORM PLATFORM_LINUX
 #endif
 
 
-#ifndef MERU_EXPORT
-# if MERU_PLATFORM == MERU_PLATFORM_WINDOWS
+#ifndef LIBPROX_EXPORT
+# if LIBPROX_PLATFORM == _PLATFORM_WINDOWS
 #   if defined(STATIC_LINKED)
-#     define MERU_EXPORT
+#     define LIBPROX_EXPORT
 #   else
-#     if defined(MERU_BUILD)
-#       define MERU_EXPORT __declspec(dllexport)
+#     if defined(LIBPROX_BUILD)
+#       define LIBPROX_EXPORT __declspec(dllexport)
 #     else
-#       define MERU_EXPORT __declspec(dllimport)
+#       define LIBPROX_EXPORT __declspec(dllimport)
 #     endif
 #   endif
 # else
 #   if defined(__GNUC__) && defined(GCC_HASVISIBILITY)
-#     define MERU_EXPORT __attribute__ ((visibility("default")))
+#     define LIBPROX_EXPORT __attribute__ ((visibility("default")))
 #   else
-#     define MERU_EXPORT
+#     define LIBPROX_EXPORT
 #   endif
 # endif
 #endif
 
-#ifndef MERU_EXPORT_C
-# define MERU_EXPORT_C extern "C" MERU_EXPORT
+#ifndef LIBPROX_EXPORT_C
+# define LIBPROX_EXPORT_C extern "C" LIBPROX_EXPORT
 #endif
 
+#include <string>
+#include <cassert>
+#include <map>
+#include <list>
+#include <vector>
 
-namespace Meru {
+namespace Prox {
 
 // numeric typedefs to get standardized types
 typedef unsigned char uchar;
-#if MERU_PLATFORM == MERU_PLATFORM_WINDOWS
+#if LIBPROX_PLATFORM == PLATFORM_WINDOWS
 typedef __int8 int8;
 typedef unsigned __int8 uint8;
 typedef __int16 int16;
@@ -70,20 +73,12 @@ typedef uint64_t uint64;
 typedef float float32;
 typedef double float64;
 
-// floats or doubles, this #define should actually come from config.h
-#define MERU_DOUBLE_PRECISION 1
-#if MERU_DOUBLE_PRECISION
-typedef float64 real;
-#else
-typedef float32 real;
-#endif
-
 typedef uchar byte;
 
 // just in case we want to swap in a different string implementation,
 // e.g. one using wchar_t
 typedef std::string String;
 
-} // namespace Meru
+} // namespace Prox
 
 #endif //_PLATFORM_HPP_
